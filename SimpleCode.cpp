be465be68a13fe5e62a27620307425e1b07f1302
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-03-13 09:17:26 +0600

    add 1 file to join into main branch

---
+++ SimpleCode.cpp
@@ -0,0 +1,10 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+printf("%d\n",6+2);
+
+return 0;
+}
---
+++ SimpleIf.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+    int marks;
+    scanf("Enter your marks: %d",&marks);
+
+    if(marks<40)
+    {
+        printf("You should try again. All the best!!!");
+    }
+    else if(marks<80)
+    {
+
+        printf("You perform better but try more better!!!");
+
+    }
+    else
+    {
+        printf("Congratulations!!! You have done so much better performance!!!!!");
+
+    }
+
+    return 0;
+
+
+}
