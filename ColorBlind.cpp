def69f98bd5b8ee2aebedcff095a52fcbbe5bced
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-08-31 22:56:47 +0600

    add

--- ColorBlind.cpp
+++ ColorBlind.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 void test()
 {
+    int n;
+    string a,b;
+    cin>>n;
+    cin>>a;
+    cin>>b;
+
+    int i=0;
+
+    for(i=0;i<n;i++){
+        if((a[i]=='R')&&(b[i]=='G'||b[i]=='B')){
+            cout<<"NO"<<endl;
+            return ;
+        }
+        swap(a[i],b[i]);
+       if((a[i]=='R')&&(b[i]=='G'||b[i]=='B')){
+            cout<<"NO"<<endl;
+            return ;
+        }
+                swap(a[i],b[i]);
+
+    }
+
+                cout<<"YES"<<endl;
+
     return;
 
 }
