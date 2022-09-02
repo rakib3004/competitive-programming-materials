5d6d52401571e2ef4ed4fe131305f1d89d80c4d7
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2022-09-02 16:25:35 +0600

    StickToTriangle.cpp implementation

---
+++ StickToTriangle.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+    int n;
+
+    cin>>n;
+
+
+    int ans = n/2;
+
+
+
+    int i;
+    int a,b,c;
+    for(i=0;i<n/2;i++){
+     if(a+b>c){
+
+     }
+    }
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+   cin>>t;
+    while(t--)
+    {
+
+        test();
+
+    }
+
+
+    return 0;
+
+}
